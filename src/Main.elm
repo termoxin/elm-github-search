17ff90f751030fa0998f914f2b86cc100a89476d
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-11 20:10:10 +0300

    Feature: search user by name

--- src/Main.elm
+++ src/Main.elm
@@ -1,22 +1,46 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, input, text)
-import Html.Attributes exposing (class, placeholder, src, type_)
-import Html.Events exposing (onInput)
+import Html exposing (Html, button, div, h1, img, input, p, text)
+import Html.Attributes exposing (alt, class, placeholder, src, type_)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode as Decode exposing (Decoder, string)
+import Json.Decode.Pipeline exposing (required)
 
 
 
 ---- MODEL ----
 
 
+type alias User =
+    { username : String
+    , name : String
+    , photoUrl : String
+    , link : String
+    }
+
+
 type alias Model =
-    { search : String }
+    { error : String
+    , search : String
+    , user : User
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { search = "" }, Cmd.none )
+    ( { error = ""
+      , search = ""
+      , user =
+            { username = ""
+            , name = ""
+            , photoUrl = ""
+            , link = ""
+            }
+      }
+    , Cmd.none
+    )
 
 
 
@@ -25,24 +49,70 @@ init =
 
 type Msg
     = EnteredSearch String
+    | GotUser (Result Http.Error User)
+    | SearchUser
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         EnteredSearch search ->
-            ( { model | search = String.reverse search }, Cmd.none )
+            ( { model | search = search }, Cmd.none )
+
+        GotUser (Ok user) ->
+            ( { model | user = user }, Cmd.none )
+
+        GotUser (Err error) ->
+            Debug.todo "Save error to show it in UI"
+                ( model, Cmd.none )
+
+        SearchUser ->
+            let
+                responseDecoder : Decoder User
+                responseDecoder =
+                    Decode.succeed User
+                        |> required "login" string
+                        |> required "name" string
+                        |> required "avatar_url" string
+                        |> required "url" string
+
+                cmd : Cmd Msg
+                cmd =
+                    Http.get
+                        { url = "https://api.github.com/users/" ++ model.search
+                        , expect = Http.expectJson GotUser responseDecoder
+                        }
+            in
+            ( model, cmd )
 
 
 
 ---- VIEW ----
 
 
+viewUser : User -> Html Msg
+viewUser user =
+    div [ class "user-container" ]
+        [ img [ src user.photoUrl, alt user.username ] []
+        , p [ class "user-name" ]
+            [ text
+                (if user.name /= "" then
+                    "Name: " ++ user.name
+
+                 else
+                    ""
+                )
+            ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ h1 [] [ text model.search ]
+        , viewUser model.user
         , input [ type_ "text", class "search-input", placeholder "Type something to search", onInput EnteredSearch ] []
+        , button [ class "search-button", onClick SearchUser ] [ text "Search" ]
         ]
 
 
